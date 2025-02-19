53b3149461612308a3d04315a67448b7505c4711
rooooooooob <raftias@gmail.com>
2025-02-18 18:34:50 -0800

    Player 2 Practice + Fixes
    You can now play practice mode as P2 - this helps for debugging P2 UI
    fixes quickly.
    
    Bugfixes:
     * P2 stance change arrows were not clickable yet were visible when not
       in neutral stance.
     * P2 Confirm button displayed after battle when not in move selection
       state

--- examples/pvp/phaser/src/battle/mockapi.ts
+++ examples/pvp/phaser/src/battle/mockapi.ts
@@ -14,9 +14,10 @@ export class MockPVPArenaAPI implements DeployedPVPArenaAPI {
     readonly state$: Observable<PVPArenaDerivedState>;
     subscriber: Subscriber<PVPArenaDerivedState> | undefined;
     mockState: PVPArenaDerivedState;
+    isP1: boolean;
 
 
-    constructor() {
+    constructor(isP1: boolean) {
         this.deployedContractAddress = 'mocked address, do not use';
         this.state$ = new Observable<PVPArenaDerivedState>((subscriber) => {
             this.subscriber = subscriber;
@@ -35,12 +36,26 @@ export class MockPVPArenaAPI implements DeployedPVPArenaAPI {
             p2Dmg: [BigInt(0), BigInt(0), BigInt(0)],
             p2Stances: [STANCE.neutral, STANCE.neutral, STANCE.neutral],
         };
+        this.isP1 = isP1;
         setTimeout(() => {
             this.subscriber?.next(this.mockState);
+            if (!isP1) {
+                setTimeout(() => {
+                    this.mockState = {
+                        ...this.mockState,
+                        p1Heroes: [generateRandomHero()],
+                        state: GAME_STATE.p2_selecting_first_heroes,
+                    },
+                    this.subscriber?.next(this.mockState);
+                }, MOCK_DELAY);
+            }
         }, MOCK_DELAY);
     }
 
     async p1_select_first_hero(first_p1_hero: Hero): Promise<void> {
+        if (!this.isP1) {
+            throw new Error('Not P1 and p1_select_first_hero() called');
+        }
         setTimeout(() => {
             this.mockState = {
                 ...this.mockState,
@@ -48,7 +63,7 @@ export class MockPVPArenaAPI implements DeployedPVPArenaAPI {
                 state: GAME_STATE.p2_selecting_first_heroes,
             };
             this.subscriber?.next(this.mockState);
-            // mock p2 select heroes
+            // mock p2 select first heroes
             setTimeout(() => {
                 this.mockState = {
                     ...this.mockState,
@@ -64,16 +79,48 @@ export class MockPVPArenaAPI implements DeployedPVPArenaAPI {
     }
   
     async p2_select_first_heroes(heroes: Hero[]): Promise<void> {
-        // should never be called (TODO: let you have a p2 testing environment too?)
-        throw new Error("do not call this");
+        if (this.isP1) {
+            throw new Error('Not P2 and p2_select_first_heroes() called');
+        }
+        setTimeout(() => {
+            this.mockState = {
+                ...this.mockState,
+                p2Heroes: heroes,
+                state: GAME_STATE.p1_selecting_last_heroes,
+            };
+            this.subscriber?.next(this.mockState);
+            // mock p1 selecting last heroes
+            setTimeout(() => {
+                this.mockState = {
+                    ...this.mockState,
+                    p1Heroes: [...this.mockState.p1Heroes, generateRandomHero(), generateRandomHero()],
+                    state: GAME_STATE.p2_selecting_last_hero,
+                };
+                this.subscriber?.next(this.mockState);
+            });
+        }, MOCK_DELAY)
     }
 
     async p2_select_last_hero(hero: Hero): Promise<void> {
-        // should never be called (TODO: let you have a p2 testing environment too?)
-        throw new Error("do not call this");
+        if (this.isP1) {
+            throw new Error('Not P2 and p2_select_first_heroes() called');
+        }
+        setTimeout(() => {
+            this.mockState = {
+                ...this.mockState,
+                p2Heroes: [...this.mockState.p2Heroes, hero],
+                state: GAME_STATE.p1_commit,
+            };
+            this.subscriber?.next(this.mockState);
+            // also mock out p1 commit
+            this.mockP1Commit();
+        }, MOCK_DELAY)
     }
 
     async p1_select_last_heroes(last_p1_heroes: Hero[]): Promise<void> {
+        if (!this.isP1) {
+            throw new Error('Not P1 and p1_select_last_heroes() called');
+        }
         setTimeout(() => {
             this.mockState = {
                 ...this.mockState,
@@ -94,6 +141,9 @@ export class MockPVPArenaAPI implements DeployedPVPArenaAPI {
     }
 
     async p1Commit(commands: bigint[], stances: STANCE[]): Promise<void> {
+        if (!this.isP1) {
+            throw new Error('Not P1 and p1Commit() called');
+        }
         console.log(`p1Commit(${safeJSONString(commands)}, ${JSON.stringify(stances)})`);
         setTimeout(() => {
             this.mockState = {
@@ -103,45 +153,26 @@ export class MockPVPArenaAPI implements DeployedPVPArenaAPI {
                 state: GAME_STATE.p2_commit_reveal,
             };
             this.subscriber?.next(this.mockState);
-            // mock p2 commit
-            setTimeout(() => {
-                // just randomly attack/move
-                // TODO: take death into account
-                this.mockState = {
-                    ...this.mockState,
-                    p2Cmds: this.mockState.p2Cmds!.map((cmd, i) => {
-                        if (this.mockState.p2Dmg[i] < MAX_HP) {
-                            const availableTargets = [0, 1, 2].filter((j) => this.mockState.p1Dmg[j] < MAX_HP);
-                            const ret = BigInt(availableTargets[Phaser.Math.Between(0, availableTargets.length - 1)]);
-                            console.log(`availableTargets(${i}) = ${availableTargets} -> ${ret}`);
-                            return ret;
-                        }
-                        // arbitrary but causes breaking errors early since dead units should never have their commands read
-                        return BigInt(1000000);
-                    }),
-                    p2Stances: this.mockState.p2Stances.map((stance, i) => {
-                        if (this.mockState.p2Dmg[i] < MAX_HP) {
-                            switch (stance) {
-                                case STANCE.defensive:
-                                    return Phaser.Math.Between(0, 1) as STANCE;
-                                case STANCE.aggressive:
-                                    return Phaser.Math.Between(1, 2) as STANCE;
-                                case STANCE.neutral:
-                                    return Phaser.Math.Between(0, 2) as STANCE;
-                            }
-                        }
-                        return stance;
-                    }),
-                    state: GAME_STATE.p1_reveal,
-                };
-                this.subscriber?.next(this.mockState);
-            }, MOCK_DELAY);
+            this.mockP2Commit();
         }, MOCK_DELAY);
     }
 
     async p2Commit(commands: bigint[], stances: STANCE[]): Promise<void> {
-        // should never be called (TODO: let you have a p2 testing environment too?)
-        throw new Error("do not call this");
+        if (this.isP1) {
+            throw new Error('Not P2 and p2Commit() called');
+        }
+        console.log(`p2Commit(${safeJSONString(commands)}, ${JSON.stringify(stances)})`);
+        setTimeout(() => {
+            this.mockState = {
+                ...this.mockState,
+                p2Cmds: commands,
+                p2Stances: stances,
+                state: GAME_STATE.p1_reveal,
+            };
+            this.subscriber?.next(this.mockState);
+            // mock p1 reveal
+            this.p1Reveal();
+        }, MOCK_DELAY);
     }
 
     async p1Reveal(): Promise<void> {
@@ -149,34 +180,108 @@ export class MockPVPArenaAPI implements DeployedPVPArenaAPI {
             console.log(`MockState: ${safeJSONString(this.mockState)}`);
             let p1Dmg = this.mockState.p1Dmg;
             let p2Dmg = this.mockState.p2Dmg;
+            const p1Alive = this.mockState.p1Dmg.map((hp) => hp < MAX_HP);
+            const p2Alive = this.mockState.p2Dmg.map((hp) => hp < MAX_HP);
             for (let i = 0; i < 3; ++i) {
-                if (this.mockState.p1Dmg[i] < MAX_HP) {
+                if (p1Alive[i]) {
                     const p1Cmd = Number(this.mockState.p1Cmds![i]);
-                    p2Dmg[p1Cmd] = BigInt(Math.min(MAX_HP, Number(p2Dmg[p1Cmd] + pureCircuits.calc_item_dmg_against(
+                    p2Dmg[p1Cmd] = this.mockState.p2Dmg[p1Cmd] + pureCircuits.calc_item_dmg_against(
                         pureCircuits.calc_stats(this.mockState.p1Heroes[i]),
                         this.mockState.p1Stances[i],
                         pureCircuits.calc_stats(this.mockState.p2Heroes[p1Cmd]),
                         this.mockState.p2Stances[p1Cmd],
-                    ))));
+                    );
                 }
-                if (this.mockState.p2Dmg[i] < MAX_HP) {
+                if (p2Alive[i]) {
                     const p2Cmd = Number(this.mockState.p2Cmds![i]);
-                    p1Dmg[p2Cmd] = BigInt(Math.min(MAX_HP, Number(p1Dmg[p2Cmd] + pureCircuits.calc_item_dmg_against(
+                    p1Dmg[p2Cmd] = this.mockState.p1Dmg[p2Cmd] + pureCircuits.calc_item_dmg_against(
                         pureCircuits.calc_stats(this.mockState.p2Heroes[i]),
                         this.mockState.p2Stances[i],
                         pureCircuits.calc_stats(this.mockState.p1Heroes[p2Cmd]),
                         this.mockState.p1Stances[p2Cmd],
-                    ))));
+                    );
                 }
             }
-            const p1Dead = p1Dmg.every((hp) => hp >= BigInt(MAX_HP));
-            const p2Dead = p2Dmg.every((hp) => hp >= BigInt(MAX_HP));
+            const allP1Dead = p1Dmg.every((hp) => hp >= BigInt(MAX_HP));
+            const allP2Dead = p2Dmg.every((hp) => hp >= BigInt(MAX_HP));
             this.mockState = {
                 ...this.mockState,
-                state: p1Dead ? (p2Dead ? GAME_STATE.tie : GAME_STATE.p2_win) : (p2Dead ? GAME_STATE.p1_win : GAME_STATE.p1_commit),
+                state: allP1Dead ? (allP2Dead ? GAME_STATE.tie : GAME_STATE.p2_win) : (allP2Dead ? GAME_STATE.p1_win : GAME_STATE.p1_commit),
                 round: this.mockState.round + BigInt(1),
                 p1Dmg,
                 p2Dmg,
+            };
+            this.subscriber?.next(this.mockState);
+            // mock out p1 commit in case p1Reeal is called mocked out from p2's commands
+            if (!this.isP1) {
+                this.mockP1Commit();
+            }
+        }, MOCK_DELAY);
+    }
+    
+    private mockP1Commit() {
+        setTimeout(() => {
+            // just randomly attack/move
+            this.mockState = {
+                ...this.mockState,
+                p1Cmds: this.mockState.p1Cmds!.map((cmd, i) => {
+                    if (this.mockState.p1Dmg[i] < MAX_HP) {
+                        const availableTargets = [0, 1, 2].filter((j) => this.mockState.p2Dmg[j] < MAX_HP);
+                        const ret = BigInt(availableTargets[Phaser.Math.Between(0, availableTargets.length - 1)]);
+                        console.log(`availableTargets(${i}) = ${availableTargets} -> ${ret}`);
+                        return ret;
+                    }
+                    // arbitrary but causes breaking errors early since dead units should never have their commands read
+                    return BigInt(1000000);
+                }),
+                p1Stances: this.mockState.p1Stances.map((stance, i) => {
+                    if (this.mockState.p1Dmg[i] < MAX_HP) {
+                        switch (stance) {
+                            case STANCE.defensive:
+                                return Phaser.Math.Between(0, 1) as STANCE;
+                            case STANCE.aggressive:
+                                return Phaser.Math.Between(1, 2) as STANCE;
+                            case STANCE.neutral:
+                                return Phaser.Math.Between(0, 2) as STANCE;
+                        }
+                    }
+                    return stance;
+                }),
+                state: GAME_STATE.p2_commit_reveal,
+            };
+            this.subscriber?.next(this.mockState);
+        }, MOCK_DELAY);
+    }
+
+    private mockP2Commit() {
+        setTimeout(() => {
+            // just randomly attack/move
+            this.mockState = {
+                ...this.mockState,
+                p2Cmds: this.mockState.p2Cmds!.map((cmd, i) => {
+                    if (this.mockState.p2Dmg[i] < MAX_HP) {
+                        const availableTargets = [0, 1, 2].filter((j) => this.mockState.p1Dmg[j] < MAX_HP);
+                        const ret = BigInt(availableTargets[Phaser.Math.Between(0, availableTargets.length - 1)]);
+                        console.log(`availableTargets(${i}) = ${availableTargets} -> ${ret}`);
+                        return ret;
+                    }
+                    // arbitrary but causes breaking errors early since dead units should never have their commands read
+                    return BigInt(1000000);
+                }),
+                p2Stances: this.mockState.p2Stances.map((stance, i) => {
+                    if (this.mockState.p2Dmg[i] < MAX_HP) {
+                        switch (stance) {
+                            case STANCE.defensive:
+                                return Phaser.Math.Between(0, 1) as STANCE;
+                            case STANCE.aggressive:
+                                return Phaser.Math.Between(1, 2) as STANCE;
+                            case STANCE.neutral:
+                                return Phaser.Math.Between(0, 2) as STANCE;
+                        }
+                    }
+                    return stance;
+                }),
+                state: GAME_STATE.p1_reveal,
             };
             this.subscriber?.next(this.mockState);
         }, MOCK_DELAY);
