993a88b8e13ced68a2b85a3e00210adc041638b2
Enzo Cioppettini <48031343+ecioppettini@users.noreply.github.com>
2025-03-12 01:16:24 -0300

    fix some miscompilations

--- examples/pvp/api/src/index.ts
+++ examples/pvp/api/src/index.ts
@@ -348,7 +348,7 @@ export class PVPArenaAPI implements DeployedPVPArenaAPI {
     console.log('before[1]');
     var txData;
     try {
-      const state = await PVPArenaAPI.getPrivateState(this.providers);
+      const state = await PVPArenaAPI.getPrivateState(this.providers.privateStateProvider);
       state!.commands = commands;
       state!.stances = stances;
       await this.providers.privateStateProvider.set('pvpPrivateState', state);
@@ -375,7 +375,7 @@ export class PVPArenaAPI implements DeployedPVPArenaAPI {
     console.log('before[2]');
     var txData;
     try {
-      const state = await PVPArenaAPI.getPrivateState(this.providers);
+      const state = await PVPArenaAPI.getPrivateState(this.providers.privateStateProvider);
       state!.commands = commands;
       state!.stances = stances;
       await this.providers.privateStateProvider.set('pvpPrivateState', state);
@@ -402,7 +402,7 @@ export class PVPArenaAPI implements DeployedPVPArenaAPI {
 
     var txData;
     try {
-      const state = await PVPArenaAPI.getPrivateState(this.providers);
+      const state = await PVPArenaAPI.getPrivateState(this.providers.privateStateProvider);
       state!.commands = commands;
       state!.stances = stances;
       await this.providers.privateStateProvider.set('pvpPrivateState', state);
@@ -438,7 +438,7 @@ export class PVPArenaAPI implements DeployedPVPArenaAPI {
       // EXERCISE ANSWER
       privateStateKey: 'pvpPrivateState', // EXERCISE ANSWER
       contract: pvpContractInstance,
-      initialPrivateState: await PVPArenaAPI.getPrivateState(providers), // EXERCISE ANSWER
+      initialPrivateState: await PVPArenaAPI.getPrivateState(providers.privateStateProvider), // EXERCISE ANSWER
       //args: [],
     });
     logger?.trace({
@@ -470,7 +470,7 @@ export class PVPArenaAPI implements DeployedPVPArenaAPI {
       contractAddress,
       contract: pvpContractInstance,
       privateStateKey: 'pvpPrivateState',
-      initialPrivateState: await PVPArenaAPI.getPrivateState(providers),
+      initialPrivateState: await PVPArenaAPI.getPrivateState(providers.privateStateProvider),
     });
 
     logger?.trace({
