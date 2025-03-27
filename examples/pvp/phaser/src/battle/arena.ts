PaimaStudios/pvp-arena

commit 51b4b7cf158bf6cfe2295a8df570417d02ce6de5

Rejoining matches (#17)
Joining an in-progress match re-joins it if you are one of the players.

examples/pvp/phaser/src/battle/arena.ts
@@ -9,6 +9,7 @@ import { HeroIndex, hpDiv, Rank, Team } from './index';
 import { Button } from '../menus/button';
 import { init } from 'fp-ts/lib/ReadonlyNonEmptyArray';
 import { closeTooltip, makeTooltip, TooltipId } from '../menus/tooltip';
+import { Subscription } from 'rxjs';
 
 export type BattleConfig = {
     isP1: boolean,
@@ -29,6 +30,7 @@ export class Arena extends Phaser.Scene
     matchStateText: Phaser.GameObjects.Text | undefined;
     round: number;
     submitButton: Button | undefined;
+    subscription: Subscription | undefined;
 
     constructor(config: BattleConfig, initialState: PVPArenaDerivedState) {
         super('Arena');
@@ -40,8 +42,10 @@ export class Arena extends Phaser.Scene
         this.matchState = MatchState.Initializing;
         this.round = 0;
         this.submitButton = undefined;
+    }
 
-        const subscription = config.api.state$.subscribe((state) => this.onStateChange(state));
+    preDestroy() {
+        this.subscription?.unsubscribe();
     }
 
     playerTeam(): Team {
@@ -121,16 +125,7 @@ export class Arena extends Phaser.Scene
         });
     }
 
-    onStateChange(state: PVPArenaDerivedState) {
-        console.log(`new state: ${safeJSONString(state)}`);
-        console.log(`NOW: ${gameStateStr(state.state)}`);
-
-        if (state.state == GAME_STATE.p1_selecting_first_hero || state.state == GAME_STATE.p2_selecting_first_heroes || state.state == GAME_STATE.p1_selecting_last_heroes || state.state == GAME_STATE.p2_selecting_last_hero) {
-            // for some reason we're getting updates here using old state that we can ignore
-            // it calls onStateChange for every state update that had previously happened on the equipment screen
-            return;
-        }
-
+    private createHeroes(state: PVPArenaDerivedState) {
         // create heroes initially
         if (this.heroes.length == 0) {
             for (let team = 0; team < 2; ++team) {
@@ -143,6 +138,19 @@ export class Arena extends Phaser.Scene
                 this.heroes.push(hero_actors);
             }
         }
+    }
+
+    onStateChange(state: PVPArenaDerivedState) {
+        console.log(`new state: ${safeJSONString(state)}`);
+        console.log(`NOW: ${gameStateStr(state.state)}`);
+
+        if (state.state == GAME_STATE.p1_selecting_first_hero || state.state == GAME_STATE.p2_selecting_first_heroes || state.state == GAME_STATE.p1_selecting_last_heroes || state.state == GAME_STATE.p2_selecting_last_hero) {
+            // for some reason we're getting updates here using old state that we can ignore
+            // it calls onStateChange for every state update that had previously happened on the equipment screen
+            return;
+        }
+
+        this.createHeroes(state);
 
         // update commands/stances/damages
         if (state.p1Cmds != undefined && state.p2Cmds != undefined/* && (state.state == GAME_STATE.p1_commit)*/) {
@@ -498,7 +506,79 @@ export class Arena extends Phaser.Scene
 
         this.setMatchState(MatchState.Initializing);
 
-        this.onStateChange(this.initialState);
+        this.createHeroes(this.initialState);
+        
+        if (this.initialState.round != BigInt(0) || this.initialState.state != GAME_STATE.p1_commit) {
+            console.log('===================re-doing initial state=========');
+            this.round = Number(this.initialState.round);
+            // we can't know previous stances for player 2 so to make the resuming consistent just default to the on-chain values
+            for (let team = 0; team < 2; ++team) {
+                const stances = team == 0 ? this.initialState.p1Stances : this.initialState.p2Stances;
+                const dmgs = team == 0 ? this.initialState.p1Dmg : this.initialState.p2Dmg;
+                for (let i = 0; i < 3; ++i) {
+                    const stance = stances[i];
+                    const hero = this.heroes[team][i];
+                    hero.stance = stance;
+                    hero.nextStance = stance;
+                    hero.setX(hero.rank.x(stance));
+                    hero.setDamageForResume(Number(dmgs[i]));
+                }
+            }
+
+            // we need to bruteforce to see what our commit was for
+            // this takes ~1-3s on my machine in dev mode, do we need further optimization?
+            // e.g. only consider valid moves
+            const guessTargetsForP1 = () => {
+                const startTime = Date.now();
+                for (let stance0 = 0; stance0 < 3; ++stance0) {
+                    for (let stance1 = 0; stance1 < 3; ++stance1) {
+                        for (let stance2 = 0; stance2 < 3; ++stance2) {
+                            for (let target0 = 0; target0 < 4; ++target0) {
+                                for (let target1 = 0; target1 < 4; ++target1) {
+                                    for (let target2 = 0; target2 < 4; ++target2) {
+                                        const commit = pureCircuits.calc_commit_for_checking(
+                                            this.initialState.secretKey,
+                                            [BigInt(target0), BigInt(target1), BigInt(target2)],
+                                            [stance0, stance1, stance2],
+                                            this.initialState.nonce!,
+                                        );
+                                        if (commit == this.initialState.commit) {
+                                            console.log(`found match [${stance0}, ${stance1}, ${stance2}]; [${target0}, ${target1}, ${target2}] in ${Date.now() - startTime}ms`);
+                                            this.heroes[0][0].setNextStance(stance0);
+                                            this.heroes[0][1].setNextStance(stance1);
+                                            this.heroes[0][2].setNextStance(stance2);
+                                            this.heroes[0][0].setTarget(new Rank(target0 as HeroIndex, 1));
+                                            this.heroes[0][1].setTarget(new Rank(target1 as HeroIndex, 1));
+                                            this.heroes[0][2].setTarget(new Rank(target2 as HeroIndex, 1));
+                                            return;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                throw new Error('could not find matching commit');
+            };
+            switch (this.initialState.state) {
+                case GAME_STATE.p2_commit_reveal:
+                    if (this.config.isP1) {
+                        guessTargetsForP1();
+                    }
+                    break;
+                case GAME_STATE.p1_reveal:
+                    if (this.config.isP1) {
+                        guessTargetsForP1();
+                    } else {
+                        for (let i = 0; i < 3; ++i) {
+                            this.heroes[1][i].setTarget(new Rank(Number(this.initialState.p2Cmds![i]) as HeroIndex, 0));
+                        }
+                    }
+                    break;
+            }
+        }
+
+        this.subscription = this.config.api.state$.subscribe((state) => this.onStateChange(state));
     }
 
     update() {

examples/pvp/api/src/common-types.ts
@@ -76,7 +76,8 @@ export type PVPArenaDerivedState = {
   readonly p2Stances: STANCE[];
   readonly round: bigint;
   readonly state: GAME_STATE;
-  // readonly p2Dmg0: bigint;
-  // readonly p2Dmg1: bigint;
-  // readonly p2Dmg2: bigint;
+  // needed for resuming games as P1
+  readonly secretKey: Uint8Array;
+  readonly nonce: Uint8Array | undefined;
+  readonly commit: bigint | undefined;
 };

examples/pvp/api/src/index.ts
@@ -198,6 +198,9 @@ export class PVPArenaAPI implements DeployedPVPArenaAPI {
           p2Dmg: [ledgerState.p2Dmg0, ledgerState.p2Dmg1, ledgerState.p2Dmg2],
           p2Alive: [ledgerState.p2Alive0, ledgerState.p2Alive1, ledgerState.p2Alive2],
           p2Stances: ledgerState.p2Stances,
+          secretKey: privateState.secretKey,
+          nonce: ledgerState.commitNonce,
+          commit: ledgerState.p1Commit,
         };
       },
     );

examples/pvp/contract/src/pvp.compact
@@ -176,6 +176,11 @@ export pure circuit calc_stats(hero: Hero): TotalStats {
     };
 }
 
+// not to be used in the circuit, but useful for UI to verify commits when resuming games
+export pure circuit calc_commit_for_checking(sk: Bytes<32>, commands: Vector<3, Uint<32>>, stances: Vector<3, STANCE>, nonce: Bytes<32>): Field {
+    return transient_hash<MoveHasher>(MoveHasher { sk, commands, stances, nonce });
+}
+
 struct MoveHasher {
     sk: Bytes<32>,
     cmds: Vector<3, Uint<32>>,

examples/pvp/phaser/src/battle/hero.ts
@@ -104,7 +104,7 @@ export class HeroActor extends Phaser.GameObjects.Container {
             arena.sound.play('select');
             const leftStance = this.leftStance()!;
             // toggle to undo moving if already in this stance
-            this.nextStance = this.nextStance == leftStance ? this.stance : leftStance;
+            this.setNextStance(this.nextStance == leftStance ? this.stance : leftStance);
             this.updateNextStanceArrow();
             closeTooltip(TooltipId.Move);
             makeTooltip(this.scene, GAME_WIDTH / 2, 100, this.rank.team == 0 ? TooltipId.MoveFurther : TooltipId.MoveCloser);
@@ -112,7 +112,7 @@ export class HeroActor extends Phaser.GameObjects.Container {
         this.right_arrow.on('pointerup', () => {
             arena.sound.play('select');
             const rightStance = this.rightStance()!;
-            this.nextStance = this.nextStance == rightStance ? this.stance : rightStance;
+            this.setNextStance(this.nextStance == rightStance ? this.stance : rightStance);
             this.updateNextStanceArrow();
             closeTooltip(TooltipId.Move);
             makeTooltip(this.scene, GAME_WIDTH / 2, 100, this.rank.team == 0 ? TooltipId.MoveCloser : TooltipId.MoveFurther);
@@ -176,13 +176,25 @@ export class HeroActor extends Phaser.GameObjects.Container {
                     alpha: 0,
                     duration: 150,
                     onComplete: () => {
-                            this.arena.sound.play('death');
-                            this.visible = false;
-                            this.arena.add.image(this.x, this.y, 'skull').setFlipX(this.rank.team == 1).setDepth(-1);
+                        this.arena.sound.play('death');
+                        this.visible = false;
+                        this.arena.add.image(this.x, this.y, 'skull').setFlipX(this.rank.team == 1).setDepth(-1);
                     }
                 });
             }
         }));
+    }
+
+    /// used for resuming game
+    public setDamageForResume(dmg: number) {
+        this.realDmg = dmg;
+        this.uiDmg = dmg;
+        this.preTurnDmg = dmg;
+        if (!this.isAlive()) {
+            this.visible = false;
+            this.arena.add.image(this.x, this.y, 'skull').setFlipX(this.rank.team == 1).setDepth(-1);
+        }
+        this.updateHpBar();
     }
 
     private updateHpBar() {
@@ -196,12 +208,16 @@ export class HeroActor extends Phaser.GameObjects.Container {
 
     public toggleNextStance() {
         if (this.nextStance == STANCE.neutral) {
-            this.nextStance = this.stance == STANCE.defensive ? STANCE.defensive : STANCE.aggressive;
+            this.setNextStance(this.stance == STANCE.defensive ? STANCE.defensive : STANCE.aggressive);
         } else if (this.nextStance == STANCE.aggressive) {
-            this.nextStance = this.stance == STANCE.aggressive ? STANCE.neutral : STANCE.defensive;
+            this.setNextStance(this.stance == STANCE.aggressive ? STANCE.neutral : STANCE.defensive);
         } else {
-            this.nextStance = STANCE.neutral;
+            this.setNextStance(STANCE.neutral);
         }
+    }
+
+    public setNextStance(stance: STANCE) {
+        this.nextStance = stance;
         this.updateNextStanceArrow();
     }
 
@@ -214,7 +230,11 @@ export class HeroActor extends Phaser.GameObjects.Container {
             if (this.nextStance == leftStance) {
                 this.left_arrow.setAlpha(1).setScale(1, 1);
             } else {
-                this.left_arrow.setAlpha(0.75).setScale(0.5, 0.5);
+                if (this.arena.matchState == MatchState.WaitingOnPlayer) {
+                    this.left_arrow.setAlpha(0.75).setScale(0.5, 0.5);
+                } else {
+                    this.left_arrow.visible = false;
+                }
             }
         }
         const rightStance = this.rightStance();
@@ -225,7 +245,11 @@ export class HeroActor extends Phaser.GameObjects.Container {
             if (this.nextStance == rightStance) {
                 this.right_arrow.setAlpha(1).setScale(1, 1);
             } else {
-                this.right_arrow.setAlpha(0.75).setScale(0.5, 0.5);
+                if (this.arena.matchState == MatchState.WaitingOnPlayer) {
+                    this.right_arrow.setAlpha(0.75).setScale(0.5, 0.5);
+                } else {
+                    this.right_arrow.visible = false;
+                }
             }
         }
         // also update the damage estimate

examples/pvp/phaser/src/battle/mockapi.ts
@@ -36,6 +36,9 @@ export class MockPVPArenaAPI implements DeployedPVPArenaAPI {
             p2Dmg: [BigInt(0), BigInt(0), BigInt(0)],
             p2Alive: [true, true, true],
             p2Stances: [STANCE.neutral, STANCE.neutral, STANCE.neutral],
+            nonce: undefined,
+            commit: undefined,
+            secretKey: new Uint8Array(),
         };
         this.isP1 = isP1;
         setTimeout(() => {

examples/pvp/phaser/src/main.tsx
@@ -1,7 +1,7 @@
 import { NetworkId, setNetworkId } from '@midnight-ntwrk/midnight-js-network-id';
 import '@midnight-ntwrk/dapp-connector-api';
 import { ITEM, RESULT, STANCE, Hero, ARMOR, pureCircuits, GAME_STATE } from '@midnight-ntwrk/pvp-contract';
-import { type PVPArenaDerivedState, type DeployedPVPArenaAPI } from '@midnight-ntwrk/pvp-api';
+import { type PVPArenaDerivedState, type DeployedPVPArenaAPI, PVPArenaAPI } from '@midnight-ntwrk/pvp-api';
 import './globals';
 import { type ContractAddress } from '@midnight-ntwrk/compact-runtime';
 import { LedgerState } from '@midnight-ntwrk/ledger';
@@ -124,6 +124,26 @@ export function rootObject(obj: Phaser.GameObjects.GameObject & Phaser.GameObjec
         obj = obj.parentContainer;
     }
     return obj;
+}
+
+export type ContractJoinInfo = {
+    config: BattleConfig;
+    state: PVPArenaDerivedState;
+}
+
+export async function joinContract(deployProvider: BrowserDeploymentManager, contractAddress: ContractAddress): Promise<ContractJoinInfo> {
+    return deployProvider.join(contractAddress).then((api) => new Promise((resolve) => {
+        const subscription = api.state$.subscribe((state) => {
+            subscription.unsubscribe();
+            resolve({
+                config: {
+                    isP1: state.isP1,
+                    api,
+                },
+                state,
+            });
+        });
+    }));
 }
 
 export enum MatchState {

examples/pvp/phaser/src/menus/equipment.ts
@@ -9,6 +9,8 @@ import { PVPArenaAPI, PVPArenaDerivedState } from '@midnight-ntwrk/pvp-api';
 import { Physics } from 'phaser';
 import { eq } from 'fp-ts';
 import { makeTooltip, TooltipId } from './tooltip';
+import { Observable } from 'rxjs/internal/Observable';
+import { Subscription } from 'rxjs';
 
 class SelectHeroActor extends Phaser.GameObjects.Container {
     hero: Hero;
@@ -297,7 +299,7 @@ class SlotSelector extends Phaser.GameObjects.Container {
     }
 
     // shifts hero's stats and updates visual elements
-    shift(cycle: number) {
+    public shift(cycle: number) {
         const max = equip_slot_max(this.slot);
         const index = (equip_slot_index(this.hero.hero, this.slot) + max + cycle) % max;
         switch (this.slot) {
@@ -306,12 +308,12 @@ class SlotSelector extends Phaser.GameObjects.Container {
                 // disallow bow / non-unarmed
                 if (this.hero.hero.lhs == ITEM.bow || this.hero.hero.rhs == ITEM.bow) {
                     this.hero.hero.rhs = ITEM.nothing;
-                    this.equip.slots.get(EQUIP_SLOT.rhs)!.refresh();
+                    this.equip.slots.get(EQUIP_SLOT.rhs)?.refresh();
                 }
                 // disallow double shields
                 if (this.hero.hero.lhs == ITEM.shield && this.hero.hero.rhs == ITEM.shield) {
                     this.hero.hero.rhs = ITEM.nothing;
-                    this.equip.slots.get(EQUIP_SLOT.rhs)!.refresh();
+                    this.equip.slots.get(EQUIP_SLOT.rhs)?.refresh();
                 }
                 break;
             case EQUIP_SLOT.rhs:
@@ -319,12 +321,12 @@ class SlotSelector extends Phaser.GameObjects.Container {
                 // disallow bow / non-unarmed
                 if (this.hero.hero.rhs == ITEM.bow || this.hero.hero.lhs == ITEM.bow) {
                     this.hero.hero.lhs = ITEM.nothing;
-                    this.equip.slots.get(EQUIP_SLOT.lhs)!.refresh();
+                    this.equip.slots.get(EQUIP_SLOT.lhs)?.refresh();
                 }
                 // disallow double shields
                 if (this.hero.hero.lhs == ITEM.shield && this.hero.hero.rhs == ITEM.shield) {
                     this.hero.hero.lhs = ITEM.nothing;
-                    this.equip.slots.get(EQUIP_SLOT.lhs)!.refresh();
+                    this.equip.slots.get(EQUIP_SLOT.lhs)?.refresh();
                 }
                 break;
             case EQUIP_SLOT.helmet:
@@ -345,7 +347,7 @@ class SlotSelector extends Phaser.GameObjects.Container {
     }
 
     // refreshes the text / hero (stats) based on current hero's stats
-    refresh() {
+    public refresh() {
         switch (this.slot) {
             case EQUIP_SLOT.lhs:
                 this.text.setText(`${equip_slot_name(this.slot)}: ${item_str(this.hero.hero.lhs)}`);
@@ -382,6 +384,7 @@ export class EquipmentMenu extends Phaser.Scene {
     selecting: number;
     selector: EquipmentSelector | undefined;
     setupStateText: Phaser.GameObjects.Text | undefined;
+    subscription: Subscription | undefined;
 
     constructor(config: BattleConfig) {
         super('EquipmentMenu');
@@ -389,12 +392,31 @@ export class EquipmentMenu extends Phaser.Scene {
         this.heroes = [];
         this.selecting = 0;
         this.setupState = config.isP1 ? SetupState.SelectingPlayerHeroes : SetupState.WaitingOnOpponent;
-        const subscription = config.api.state$.subscribe((state) => this.onStateChange(state));
+    }
+
+    preDestroy() {
+        this.subscription?.unsubscribe();
     }
 
     onStateChange(state: PVPArenaDerivedState) {
         console.log(`new state: ${safeJSONString(state)}`);
         console.log(`NOW: ${gameStateStr(state.state)}`);
+
+        // when joining this.selecting could be out of date
+        switch (state.state) {
+            case GAME_STATE.p1_selecting_first_hero:
+                this.selecting = 0;
+                break;
+            case GAME_STATE.p1_selecting_last_heroes:
+                this.selecting = this.config.isP1 ? 1 : 2;
+                break;
+            case GAME_STATE.p2_selecting_first_heroes:
+                this.selecting = this.config.isP1 ? 1 : 0;
+                break;
+            case GAME_STATE.p2_selecting_last_hero:
+                this.selecting = 2;
+                break;
+        }
 
         // update heroes
         let tweens: Phaser.Types.Tweens.TweenBuilderConfig[] = [];
@@ -441,6 +463,8 @@ export class EquipmentMenu extends Phaser.Scene {
                 break;
             case GAME_STATE.p1_commit:
                 // game started
+                console.log(`================skipping equip screen=============`);
+                // This causes black screens when uncommented. TODO: investigate. Issue: https://github.com/PaimaStudios/pvp-arena/issues/22
                 //this.scene.remove('Arena');
                 this.scene.add('Arena', new Arena(this.config, state));
                 this.scene.start('Arena');
@@ -494,6 +518,8 @@ export class EquipmentMenu extends Phaser.Scene {
             }
             this.heroes.push(heroes);
         }
+
+        this.subscription = this.config.api.state$.subscribe((state) => this.onStateChange(state));
     }
 
     // advances to next step

commit d42e9afe72023931f755c482d4e8e71736b23d5a

Offline practice mode fix
Resolves bug introduced in #17 where 1st turn (as 1P only) would never
resolve when starting an offline practice match.

examples/pvp/phaser/src/battle/arena.ts
@@ -506,10 +506,9 @@ export class Arena extends Phaser.Scene
 
         this.setMatchState(MatchState.Initializing);
 
-        this.createHeroes(this.initialState);
-        
         if (this.initialState.round != BigInt(0) || this.initialState.state != GAME_STATE.p1_commit) {
-            console.log('===================re-doing initial state=========');
+            this.createHeroes(this.initialState);
+
             this.round = Number(this.initialState.round);
             // we can't know previous stances for player 2 so to make the resuming consistent just default to the on-chain values
             for (let team = 0; team < 2; ++team) {
@@ -576,6 +575,8 @@ export class Arena extends Phaser.Scene
                     }
                     break;
             }
+        } else {
+            this.onStateChange(this.initialState);
         }
 
         this.subscription = this.config.api.state$.subscribe((state) => this.onStateChange(state));